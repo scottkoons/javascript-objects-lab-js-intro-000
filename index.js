5ea98a2bed3bc86210724acbc5356aa866ecd0d3
communal-documentation-0347 <scottkoons@gmail.com>
2020-07-28 23:09:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return object[key] = value;
-  // return object;
+  object[key] = value;
+  return object;
 }
 
 function deleteFromObjectByKey(object, key) {
